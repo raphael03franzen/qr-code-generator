a785017299b3b2aa5cb6a6c163ae236c4ae29050
Raphael Franzen <franzen.raphael@hotmail.com>
2025-07-18 13:53:49 +0200

    Refactor Finder pattern generator to own function

--- src/qr.c
+++ src/qr.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
+int addFinderPattern(FILE*);
 int main(int argc, char** argv){
 	FILE *file = fopen("test.svg", "w");
 
@@ -7,13 +8,16 @@ int main(int argc, char** argv){
 		printf("Error opening file!\n");
 		return 1; // Exit with an error code
 	}
-	fprintf(file,"<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"210\" height=\"210\" viewBox=\"0 0 210 210\">");
-	fprintf(file,"<path fill-rule=\"evenodd\" d=\"M0 0h70v70H0m10-60h50v50H10m10-40h30v30H20\" id=\"a\"/>");
-	fprintf(file,"\t<use href=\"#a\" x=\"140\"/>\n");
-	fprintf(file,"\t<use href=\"#a\" y=\"140\"/>\n");
-	fprintf(file,"</svg>\n");
+	addFinderPattern(file);
 	fclose(file);
 
 	return 0; // Exit successfully
 }
 
+int addFinderPattern(FILE* file){
+	fprintf(file,"<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"210\" height=\"210\" viewBox=\"0 0 210 210\">");
+	fprintf(file,"<path fill-rule=\"evenodd\" d=\"M0,0h70v70H0M10,10h50v50H10M20,20h30v30H20\" id=\"a\"/>");
+	fprintf(file,"<use href=\"#a\" x=\"140\"/>");
+	fprintf(file,"<use href=\"#a\" y=\"140\"/>");
+	fprintf(file,"</svg>");
+}
